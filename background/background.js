ede7be57f17c6c21a95a7d9ba9f8c393bd3b8333
Imonikai <108214765+Imonikai@users.noreply.github.com>
2023-10-31 17:42:21 +0900

    fix: 設定が保存できなくなる問題に対処（更新したら設定は初期化される）

--- background/background.js
+++ background/background.js
@@ -1,11 +1,10 @@
 /* 設定の初期値 */
-let mySettings = {
+const mySettings = {
     fixAttendanceTable: false,
     emphasisOnPassing: false,
 };
 
 /* 拡張機能がインストールされたら設定の初期値をセットする */
 chrome.runtime.onInstalled.addListener(async () => {
-    mySettings = (await chrome.storage.local.get('settings')).settings;
     chrome.storage.local.set({ settings: mySettings });
 });
