5cb0c8c8408b5d49c32df0a70881bfebb7b46727
imonikai <108214765+imonikai@users.noreply.github.com>
2023-12-11 12:39:00 +0900

    fix?: chromeが更新されたりすると設定がリセットされる？問題？を対処？

--- background/background.js
+++ background/background.js
@@ -1,10 +1,16 @@
-/* 設定の初期値 */
-const mySettings = {
-    fixAttendanceTable: false,
-    emphasisOnPassing: false,
-};
-
 /* 拡張機能がインストールされたら設定の初期値をセットする */
 chrome.runtime.onInstalled.addListener(async () => {
+    /* 設定の初期値 */
+    let mySettings = {
+        fixAttendanceTable: false,
+        emphasisOnPassing: false,
+    };
+
+    /* すでに設定が存在していたならその設定をセットする */
+    const tmpSettings = (await chrome.storage.local.get('settings')).settings;
+    if (tmpSettings !== undefined) {
+        mySettings = tmpSettings;
+    }
+
     chrome.storage.local.set({ settings: mySettings });
 });
